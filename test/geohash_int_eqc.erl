922675f6175a4eb3320b7857207f2465662873f6
wulei <wulei@digitalearth.cn>
2015-09-19 23:38:06 +0800

    quickcheck test: just for 2 corner-cell pointers

--- test/geohash_int_eqc.erl
+++ test/geohash_int_eqc.erl
@@ -4,24 +4,37 @@
 
 -compile(export_all).
 
-prop_hashint_at_leftbottom_cell() ->
-    ?FORALL({X,Y,L,World}, position_in_leftbottom_cell(),
+prop_hashint_at_corners() ->
+    ?FORALL({LeftBottom,RightTop,Level,World}, position_in_corners(world()),
             begin
-                {ok, Hash} = geohash_int:encode(World,Y,X,L),
-                #{bits:=HashInt, level:=Level} = Hash,
-                0 =< HashInt andalso HashInt =< 3 andalso Level == L
+                {Longitude0, Latitude0} = LeftBottom,
+                {ok, Hash0} = geohash_int:encode(World,Latitude0,Longitude0,Level),
+                {Longitude3, Latitude3} = RightTop,
+                {ok, Hash3} = geohash_int:encode(World,Latitude3,Longitude3,Level),
+                #{bits:=HashInt0, level:=L0} = Hash0,
+                #{bits:=HashInt3, level:=L3} = Hash3,
+                Level == L0 
+                andalso HashInt0 == 0
+                andalso HashInt3 == ((1 bsl (Level*2)) - 1)
             end).
 
-position_in_leftbottom_cell() ->
-    ?LET(Level, eqc_gen:choose(1,36),
+position_in_corners(Wgen) ->
+    ?LET({Level,World}, {eqc_gen:choose(1,36),Wgen},
+         begin
+             #{north:=N,south:=S,west:=W,east:=E} = World,
+             X = (E - W) / math:pow(2, Level),
+             Y = (N - S) / math:pow(2, Level),
+             LeftBottom = {W+X/2, S+Y/2},
+             RightTop = {E - X/2, N - Y/2},
+             {LeftBottom, RightTop, Level,World}
+         end).
+
+world() ->
+    ?LET(R,real(),
          begin
             {ok, World} = geohash_int:define_world(-20037726.37,20037726.37,
                                                    -20037726.37,20037726.37,
                                                   'N'),
-             #{north:=N,south:=S,west:=W,east:=E} = World,
-             X = (E - W) / math:pow(2, Level),
-             Y = (N - S) / math:pow(2, Level),
-             {W+X/2, S+Y/2, Level,World}
+            World
          end).
-
 
