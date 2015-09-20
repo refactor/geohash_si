03d151722af421dd8d6996ef22ab23368c7208ea
wulei <wulei@digitalearth.cn>
2015-09-20 11:22:16 +0800

    quickcheck: test a random position

--- test/geohash_int_eqc.erl
+++ test/geohash_int_eqc.erl
@@ -5,17 +5,24 @@
 -compile(export_all).
 
 prop_hashint_at_corners() ->
-    ?FORALL({LeftBottom,RightTop,Level,World}, position_in_corners(world()),
+    ?FORALL({LeftBottom,RightTop,RandomPosition,Level,World}, position_in_corners(world()),
             begin
                 {Longitude0, Latitude0} = LeftBottom,
                 {ok, Hash0} = geohash_int:encode(World,Latitude0,Longitude0,Level),
+                #{bits:=HashInt0, level:=L0} = Hash0,
+
                 {Longitude3, Latitude3} = RightTop,
                 {ok, Hash3} = geohash_int:encode(World,Latitude3,Longitude3,Level),
-                #{bits:=HashInt0, level:=L0} = Hash0,
                 #{bits:=HashInt3, level:=L3} = Hash3,
-                Level == L0 
+
+                {RandLon, RandLat} = RandomPosition,
+                {ok, RandHash} = geohash_int:encode(World, RandLat, RandLon, Level),
+                #{bits:=RHashInt, level:=RL} = RandHash,
+
+                Level == L0 andalso Level == L3 andalso Level == RL
                 andalso HashInt0 == 0
                 andalso HashInt3 == ((1 bsl (Level*2)) - 1)
+                andalso HashInt0 =< RHashInt andalso RHashInt =< HashInt3
             end).
 
 position_in_corners(Wgen) ->
@@ -26,7 +33,9 @@ position_in_corners(Wgen) ->
              Y = (N - S) / math:pow(2, Level),
              LeftBottom = {W+X/2, S+Y/2},
              RightTop = {E - X/2, N - Y/2},
-             {LeftBottom, RightTop, Level,World}
+             RX = W + (E - W) * rand:uniform(),
+             RY = S + (N - S) * rand:uniform(),
+             {LeftBottom, RightTop, {RX,RY}, Level,World}
          end).
 
 world() ->
