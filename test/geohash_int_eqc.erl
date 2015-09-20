89862cbe756a0dd2323c44ec49f230789cc5da48
wulei <wulei@digitalearth.cn>
2015-09-20 15:42:51 +0800

    quickcheck: add 2 test points which positioned in 2nd and 4th quadrant!
    make sure encoding correct in different mode: z & n

--- test/geohash_int_eqc.erl
+++ test/geohash_int_eqc.erl
@@ -26,7 +26,7 @@ encode_generator(M) ->
     ?LET(P, position_in_corners(world(M)),
          do_encode(P)).
 
-do_encode({LeftBottom,RightTop,CenterLB,CenterRT,RandomPosition,Level,World}) ->
+do_encode({LeftBottom,RightTop,CenterLB,CenterRT,SecondQuadrantPoint,FourthQuadrantPoint,RandomPosition,Level,World}) ->
     {Longitude0, Latitude0} = LeftBottom,
     {Time0, {ok, Hash0}} = timer:tc(geohash_int, encode, [World,Latitude0,Longitude0,Level]),
     #{bits:=HashInt0, level:=L0} = Hash0,
@@ -47,13 +47,29 @@ do_encode({LeftBottom,RightTop,CenterLB,CenterRT,RandomPosition,Level,World}) ->
     {Time2, {ok,Hash2}} = timer:tc(geohash_int, encode, [World, CenterRTLat,CenterRTLon,Level]),
     #{bits:=HashInt2, level:=L2} = Hash2,
 
+    {SQPLon, SQPLat} = SecondQuadrantPoint,
+    {Time4,{ok,Hash4}} = timer:tc(geohash_int, encode, [World,SQPLat,SQPLon,Level]),
+    #{bits:=HashInt4, level:=L4} = Hash4,
+
+    {FQPLon, FQPLat} = FourthQuadrantPoint,
+    {Time5,{ok,Hash5}} = timer:tc(geohash_int, encode, [World,FQPLat,FQPLon,Level]),
+    #{bits:=HashInt5, level:=L5} = Hash5,
+
+    #{mode:=M} = World,
+    Start1 = 0, End1 = (1 bsl ((Level-1) * 2)) - 1,
+    Start2 = End1 + 1, End2 = (1 bsl ((Level-1) * 2)) * 2 - 1,
+    Start3 = End2 + 1, End3 = ((1 bsl (Level-1) * 2)) * 3 - 1,
+    Start4 = End3 + 1, End4 = (1 bsl (Level*2)) - 1,
+
     {Level == RL andalso HashInt0 =< RHashInt andalso RHashInt =< HashInt3
      andalso L0 == Level andalso HashInt0 == 0
-     andalso L1 == Level andalso HashInt1 == ((1 bsl ((Level-1)*2)) - 1)
-     andalso L2 == Level andalso HashInt2 == ((1 bsl ((Level-1)*2)) * 3)
-     andalso L3 == Level andalso HashInt3 == ((1 bsl (Level*2)) - 1)
+     andalso L1 == Level andalso HashInt1 == End1
+     andalso L2 == Level andalso HashInt2 == Start4
+     andalso L3 == Level andalso HashInt3 == End4
+     andalso L4 == Level andalso (((M == 'N') and (Start2 =< HashInt4 andalso HashInt4 =< End2)) orelse ((M == 'Z') and (Start3 =< HashInt4 andalso HashInt4 =< End3)))
+     andalso L5 == Level andalso (((M == 'N') and (Start3 =< HashInt5 andalso HashInt5 =< End3)) orelse ((M == 'Z') and (Start2 =< HashInt5 andalso HashInt5 =< End2)))
      ,
-     [Time0,Time1,Time2,Time3,Time]}.
+     [Time0,Time1,Time2,Time3,Time4,Time5,Time]}.
 
 
 position_in_corners(Wgen) ->
@@ -71,9 +87,11 @@ position_in_corners(Wgen) ->
                          CenterY - CellHight * rand:uniform()},
              CenterRT = {CenterX + CellWidth * rand:uniform(),
                          CenterY + CellHight * rand:uniform()},
+             SecondQuadrantPoint = {(W + CenterX) / 2, (CenterY + N) / 2},
+             FourthQuadrantPoint = {(CenterX + E) / 2, (S + CenterY) / 2},
              RX = W + (E - W) * rand:uniform(),
              RY = S + (N - S) * rand:uniform(),
-             {LeftBottom, RightTop, CenterLB, CenterRT, {RX,RY}, Level,World}
+             {LeftBottom, RightTop, CenterLB, CenterRT, SecondQuadrantPoint, FourthQuadrantPoint, {RX,RY}, Level,World}
          end).
 
 world(M) ->
